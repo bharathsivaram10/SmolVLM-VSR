4136c7307527f11dd3050a39bf57ddf27119d6b4
Sivaram <bharathsivaram@tamu.edu>
2025-02-15 18:44:09 -0600

    changes to setup and README

--- README.md
+++ README.md
@@ -12,7 +12,19 @@ I ran this using an A100 on https://lambdalabs.com/. I don't recommend anything 
 Run the bash file to update some packages and also download the dataset repo + images
 
 ```bash
+cd SmolVLM-VSR
 bash setup.sh
 ```
 
-You can now follow the steps in the notebook!
+Sometimes, the Dropbox link used in the setup.sh file can fail due to too many downloads in that day. In that case go into the visual-spatial-reasoning/data directory
+and follow the instructions to download COCO data and run the select_only_revlevant_images.py script. I suggest saving the images locally so you can skip the image download step in future.
+
+If you have already downloaded the images and just want to get dependencies, run with the skip images option:
+
+```bash
+bash setup.sh --skip-images
+```
+
+
+
+You can now follow the along in the notebook!
--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Parse command-line arguments
+SKIP_IMAGES=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --skip-download) SKIP_IMAGES=true ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 # Upgrade Jupyter and IPython
 pip install --upgrade jupyter
 pip install --upgrade jinja2
@@ -13,8 +24,13 @@ pip install flash-attn --no-build-isolation
 # Clone the Visual Spatial Reasoning dataset repository
 git clone https://github.com/cambridgeltl/visual-spatial-reasoning.git
 
-# Download and extract images
-wget -O vsr_images.zip "https://www.dropbox.com/scl/fi/efvlqxp4zhxfp60m1hujd/vsr_images.zip?rlkey=3w3d8dxbt7xgq64pyh7zosnzm&e=1&dl=0"
-unzip vsr_images.zip -d visual-spatial-reasoning/
+# Conditionally download and extract images
+if [ "$SKIP_IMAGES" = false ]; then
+    echo "Downloading dataset..."
+    wget -O vsr_images.zip "https://www.dropbox.com/scl/fi/efvlqxp4zhxfp60m1hujd/vsr_images.zip?rlkey=3w3d8dxbt7xgq64pyh7zosnzm&e=1&dl=0"
+    unzip vsr_images.zip -d visual-spatial-reasoning/
+else
+    echo "Skipping dataset download"
+fi
 
 unzip SmolVLM-Instruct-VSR-Saved.zip
